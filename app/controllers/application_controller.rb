aa9370498f52c6bcadca8ef14e3b26b030f0d408
takuya0323 <132263806+takuya0323@users.noreply.github.com>
2023-08-03 20:50:10 +0900

    エラーのため、endを追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -15,3 +15,4 @@ class ApplicationController < ActionController::Base
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname,:last_name,:first_name,:last_name_kana,:first_name_kana,:birthday])
   end
+end
