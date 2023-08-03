ed1f3e32ea9dc2f1def5b087edc5de58f0bdb9b2
takuya0323 <132263806+takuya0323@users.noreply.github.com>
2023-08-03 16:21:29 +0900

    ストロングパラメーターとバリデーションの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+   validates :nickname,:last_name,:first_name,:last_name_kana,:first_name_kana,:birthday, presence: true
+   validates :nickname, length: { maximum: 40 }
+
 end
