69f3bdf905f76764f621573345f4767f9737e727
takuya0323 <132263806+takuya0323@users.noreply.github.com>
2023-08-07 15:04:02 +0900

    userモデル単体テストコードの記述

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -4,7 +4,7 @@ FactoryBot.define do
       person {Gimei.name}
     end
     nickname              {Faker::Name.name}
-    email                 {Faker::Internet.unique.free_email}
+    email                 {Faker::Internet.free_email}
     password              {Faker::Internet.password(min_length: 6)}
     password_confirmation {password}
     last_name             { person.last.kanji }
