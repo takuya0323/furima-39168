b839ed3008b442ecbef6cd9670badf5fb8672533
takuya0323 <132263806+takuya0323@users.noreply.github.com>
2023-08-08 14:41:22 +0900

    コメントの修正３

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,5 +7,5 @@ class User < ApplicationRecord
    validates :nickname, length: { maximum: 40 }, presence:true
    validates :password, format: { with:/\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i }
    validates :last_name, :first_name, format: { with:/\A[ぁ-んァ-ヶ一-龥々ー]+\z/}, presence: true
-   validates :last_name_kana, :first_name_kana, format: {with:/\A[ァ-ヶ一]+\z/}, presence: true
+   validates :last_name_kana, :first_name_kana, format: {with:/\A[ァ-ヶー]+\z/}, presence: true
 end
