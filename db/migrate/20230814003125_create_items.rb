4f5271a6b947393e651317d2b41c92b130373368
takuya0323 <132263806+takuya0323@users.noreply.github.com>
2023-08-14 10:08:17 +0900

    DB作成及びgemインストール

---
+++ db/migrate/20230814003125_create_items.rb
@@ -0,0 +1,22 @@
+class CreateItems < ActiveRecord::Migration[6.0]
+  def change
+    create_table :items do |t|
+
+      t.timestamps
+
+      t.references :user, null: false, foreign_key: true
+      t.string :name, null: false
+      t.text :information, null: false
+      t.integer :category_id ,null: false
+      t.integer :item_status_id ,null: false
+      t.integer :shipping_cost_id ,null: false
+      t.integer :shipping_date_id ,null: false
+      t.integer :prefecture_id ,null: false
+      t.integer :price ,null: false
+
+
+    end
+  end
+end
+
+
