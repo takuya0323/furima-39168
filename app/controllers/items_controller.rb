3d554faf29fe26106f81c2a1cf0e264f2ab98691
takuya0323 <132263806+takuya0323@users.noreply.github.com>
2023-08-18 15:58:48 +0900

    エラーメッセージの表示の修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -22,8 +22,8 @@ class ItemsController < ApplicationController
   end
 
   def edit
-    if @item.user_id == current_user.id
     @item = Item.find(params[:id])
+    if @item.user_id == current_user.id
     else
       redirect_to root_path
     end
