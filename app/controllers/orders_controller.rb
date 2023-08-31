cfdc95ea9503e8a55cfe376de80ac1364f50a5b6
takuya0323 <132263806+takuya0323@users.noreply.github.com>
2023-08-31 12:02:12 +0900

    Formオブジェクトの実装とエラーの修正

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -34,9 +34,8 @@ class OrdersController < ApplicationController
   end
 
   def non_purchased_item
-    # itemがあっての、order_form（入れ子構造）。他のコントローラーで生成されたitemを使うにはcreateアクションに定義する。
     @item = Item.find(params[:item_id])
-    redirect_to root_path if current_user.id == @item.user_id || @item.order.present?
+    redirect_to root_path if current_user.id == @item.user_id 
   end
 
 end
--- app/models/order_destination.rb
+++ app/models/order_destination.rb
@@ -1,4 +1,4 @@
-class OrderDonetion
+class OrderDestination
   include ActiveModel::Model
   attr_accessor :user_id, :item_id, :postcode, :prefecture_id, :city, :block, :building, :phone_number, :token
   with_options presence: true do
