f5840341bba4ada25d30954373f9ad7dbdac34f1
takuya0323 <132263806+takuya0323@users.noreply.github.com>
2023-08-31 14:16:03 +0900

    テストコードの記述

---
+++ spec/models/order_destination_spec.rb
@@ -0,0 +1,78 @@
+require 'rails_helper'
+
+RSpec.describe OrderDestination, type: :model do
+  before do
+    @order_destination = FactoryBot.build(:order_destination)
+  end
+
+  describe '配送先情報の保存' do
+    context '配送先情報の保存ができるとき' do
+      it 'すべての値が正しく入力されていれば保存できること' do
+        expect(@order_destination).to be_valid
+      end
+    end
+
+    context '配送先情報の保存ができないとき' do
+      it 'user_idが空だと保存できない' do
+        @order_destination.user_id = nil
+        @order_destination.valid?
+        expect(@order_destination.errors.full_messages).to include("User can't be blank")
+      end
+      it 'item_idが空だと保存できない' do
+        @order_destination.item_id = nil
+        @order_destination.valid?
+        expect(@order_destination.errors.full_messages).to include("Item can't be blank")
+      end
+      it '郵便番号が空だと保存できないこと' do
+        @order_destination.postcode = nil
+        @order_destination.valid?
+        expect(@order_destination.errors.full_messages).to include("Postcode can't be blank", 'Postcode is invalid. Include hyphen(-)')
+      end
+      it '郵便番号にハイフンがないと保存できないこと' do
+        @order_destination.postcode = 1_234_567
+        @order_destination.valid?
+        expect(@@order_destination.errors.full_messages).to include('Postcode is invalid. Include hyphen(-)')
+      end
+      it '都道府県が「---」だと保存できないこと' do
+        @order_destination.prefecture_id = 1
+        @order_destination.valid?
+        expect(@order_destination.errors.full_messages).to include("Prefecture can't be blank")
+      end
+      it '都道府県が空だと保存できないこと' do
+        @order_destination.prefecture_id = nil
+        @order_destination.valid?
+        expect(@order_destination.errors.full_messages).to include("Prefecture can't be blank")
+      end
+      it '市区町村が空だと保存できないこと' do
+        @order_destination.city = nil
+        @order_destination.valid?
+        expect(@order_destination.errors.full_messages).to include("City can't be blank")
+      end
+      it '番地が空だと保存できないこと' do
+        @order_destination.block = nil
+        @order_destination.valid?
+        expect(@order_form.errors.full_messages).to include("Block can't be blank")
+      end
+      it '電話番号が空だと保存できないこと' do
+        @order_destination.phone_number = nil
+        @order_destination.valid?
+        expect(@order_destination.errors.full_messages).to include("Phone number can't be blank")
+      end
+      it '電話番号にハイフンがあると保存できないこと' do
+        @order_destination.phone_number = '123 - 1234 - 1234'
+        @order_destination.valid?
+        expect(@order_destination.errors.full_messages).to include('Phone number is invalid')
+      end
+      it '電話番号が12桁以上あると保存できないこと' do
+        @order_destination.phone_number = 12_345_678_910_123_111
+        @order_destination.valid?
+        expect(@order_destination.errors.full_messages).to include('Phone number is invalid')
+      end
+      it 'トークンが空だと保存できないこと' do
+        @order_destination.token = nil
+        @order_destination.valid?
+        expect(@order_destination.errors.full_messages).to include("Token can't be blank")
+      end
+    end
+  end
+end
